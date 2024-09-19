3f8012ce6c0e665ce6d5a77704d0eee0be1fad42
LuckGets <kasdies12@gmail.com>
2024-09-19 20:21:09 +0700

    setup_temp-signup

--- router/user.js
+++ router/user.js
@@ -1,3 +1,5 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 const express = require("express");
 
 const userRouter = express.Router();
@@ -18,6 +20,37 @@ userRouter.patch("/", (req, res, next) => {
   res.status(200).json({ pathchEmail: email });
 });
 
-userRouter.post("/signup", (req, res, next) => {});
+userRouter.post("/signup", async (req, res, next) => {
+  const { firstname, lastname, email, phone } = req.body;
+
+  const signupObj = { firstname, lastname, email, phone };
+
+  const isUserExist = await prisma.user.findFirst({
+    where: {
+      OR: [
+        {
+          email: email,
+        },
+        {
+          phone: phone,
+        },
+      ],
+    },
+  });
+
+  if (isUserExist) {
+    return res.status(400).json({ message: "User already exist" });
+  }
+
+  // SELECT * FROM USER WHERE email = email OR phone = phone
+
+  const user = await prisma.user.create({ data: signupObj });
+
+  if (user) {
+    res
+      .status(201)
+      .json({ message: "The user was created successfully!", user });
+  }
+});
 
 module.exports = userRouter;
